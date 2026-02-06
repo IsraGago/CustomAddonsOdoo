b2bbe71ba7275ed05cc56f2b3372c9820cbaaaec
IsraGago <igagoacun@ieschandomonte.edu.es>
2026-02-06 21:53:21 +0100

    Módulo de alquiler creado

--- alquilerv2/__manifest__.py
+++ alquilerv2/__manifest__.py
@@ -2,34 +2,29 @@
 {
     'name': "alquilerv2",
 
-    'summary': "Sistema de alquiler de productos",
+    'summary': "Modulo para el alquiler de productos",
 
     'description': """
-Long description of module's purpose
+Modulo para gestionar el alquiler de productos, permitiendo controlar disponibilidad, reservas y devoluciones.
     """,
 
-    'author': "Israel Benjamín Gago Acuña",
+    'author': "ISRAEL BENJAMÍN GAGO ACUÑA",
     'website': "https://www.israelgago.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Tools',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock','product'],
+    'depends': ['base','product'],
 
     # always loaded
-    "data": [
-        "security/ir.model.access.csv",
-        "views/alquiler_reserva_views.xml",
-        "views/templates.xml",
-        "views/views.xml"
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/product_inherit_views.xml',
     ],
 }
 
--- alquilerv2/models/models.py
+++ alquilerv2/models/models.py
@@ -1,53 +1,43 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields,api
+from odoo.exceptions import ValidationError
 
-from odoo import models, fields, api
+class Alquiler(models.Model):
+    _name = 'alquilerv2.alquiler'
+    _description = 'Registro de Alquiler'
 
-class AlquilerReserva(models.Model):
-    _name = "alquiler.reserva"
-    _description = "Contrato de Alquiler de productos"
+    nombre = fields.Char(string='Referencia', required=True)
+    cliente_id = fields.Many2one('res.partner', string='Cliente', required=True)
+    fecha_inicio = fields.Date(string='Fecha Inicio', required=True)
+    fecha_fin = fields.Date(string='Fecha Fin', required=True)
     
-    name = fields.Char(string = 'Referencia',required = True,copy = False,readonly=True,default='Nuevo')
-    partner_id = fields.Many2one('res.partner',string="Cliente",required = True)
-    date_start = fields.Date(string='Fecha de Inicio',default = fields.Date.today,required = True)
-    date_end = fields.Date(string="Fecha de Fin",required = True)
-    
-    alquiler_ids = fields.One2many('alquiler.linea','alquiler_id',string='Líneas de alquiler')
-    
-    state = fields.Selection([
-        ('draft', 'Borrador'),
-        ('confirmed', 'Confirmado'),
-        ('done', 'Finalizado'),
-        ('cancel', 'Cancelado'),
-    ], string='Estado', default='draft')
-    
-    total_cost = fields.Float(string='Coste Total', compute ='_compute_total_cost',store=True)
-    @api.depends('alquiler_ids.price_unit', 'alquiler_ids.quantity', 'date_start', 'date_end')
-    def _compute_total_cost(self):
-        for rec in self:
-            dias = 0
-            if rec.date_start and rec.date_end:
-                delta = rec.date_end - rec.date_start
-                dias = delta.days if delta.days > 0 else 1 # Mínimo 1 día
-            
-            total_lineas = sum(line.price_unit * line.quantity for line in rec.alquiler_ids)
-            rec.total_cost = total_lineas * dias
+    # Relación con las líneas: apunta al modelo hijo y al campo que lo vincula
+    lista_lineas = fields.One2many('alquilerv2.linea', 'alquiler_id', string='Productos')
 
-class AlquilerLinea(models.Model):
-    _name = "alquiler.linea"
-    _description = "Línea de alquiler"
+class Linea(models.Model):
+    _name = 'alquilerv2.linea'
+    _description = 'Linea de Alquiler'
+
+    # Campo que vincula la línea con el Alquiler padre
+    alquiler_id = fields.Many2one('alquilerv2.alquiler', string='Alquiler')
     
-    alquiler_id = fields.Many2one('alquiler.reserva',string='Reserva',required = True,ondelete='cascade')
-    product_id = fields.Many2one('product.product',string='Producto',required = True,domain=[('is_rentable', '=', True)])
-    quantity = fields.Integer(string='Cantidad',default=1,required = True)
-       
-    price_unit = fields.Float(
-    related='product_id.rental_price', 
-    string='Precio por día', 
-    readonly=False,
-    store=True
-)
-  
-class ProductTemplate(models.Model):
+    producto_id = fields.Many2one('product.product', string='Producto', required=True)
+    cantidad = fields.Float(string='Cantidad', required=True,default=1.0)
+    precio = fields.Float(string='Precio por día', required=True)
+    subtotal = fields.Float(string='Subtotal', compute='_calcular_subtotal')
+
+    @api.depends('cantidad', 'precio')
+    def _calcular_subtotal(self):
+        for registro in self:
+            registro.subtotal = registro.cantidad * registro.precio * ((registro.alquiler_id.fecha_fin - registro.alquiler_id.fecha_inicio).days + 1)
+
+    @api.constrains('fecha_inicio', 'fecha_fin')
+    def _check_fechas(self):
+        for registro in self:
+            if registro.fecha_inicio and registro.fecha_fin:
+                if registro.fecha_inicio > registro.fecha_fin:
+                    raise ValidationError(_('¡Error! La fecha de inicio no puede ser posterior a la fecha de fin.'))
+
+class Producto(models.Model):
     _inherit = 'product.template'
-    is_rentable = fields.Boolean(string='Es alquilable', default=False)
-    rental_price = fields.Float(string='Precio de alquiler por día', default=0.0)
+
+    es_alquilable = fields.Boolean(string='Es alquilable')
